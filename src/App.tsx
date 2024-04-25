e8deed53e43e74ecab88359ee7e1caaafe8e17f2
ParkKiHun <miy1205@naver.com>
2024-04-25 11:19:40 +0900

    error : vite 환경변수 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Layout from "components/layout";
-import Home from "routes/home";
-import Profile from "routes/Profile";
-import Login from "routes/login";
-import CreateAccount from "routes/create-account";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/Profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import { useEffect, useState } from "react";
-import LoadingScreen from "components/loading-screen";
-import { auth } from "firebase";
+import LoadingScreen from "./components/loading-screen";
+import { auth } from "./firebase";
 
 const GlobalStyles = createGlobalStyle`
 ${reset};
--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,3 +1,55 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 420px;
+  padding: 50px 0px;
+`;
+const Form = styled.form`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+`;
+const Title = styled.h1`
+  font-size: 42px;
+`;
+const Input = styled.input`
+  padding: 10px 20px;
+  border-radius: 50px;
+  border: none;
+  width: 100%;
+  font-size: 16px;
+  &[type="submit"] {
+    cursor: pointer;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
+const Error = styled.span`
+  font-weight: 600;
+  color: tomato;
+`;
+
 export default function CreateAccount() {
-  return <h1>create-account</h1>;
+  return (
+    <Wrapper>
+      <Form>
+        <Input name="name" placeholder="Name" type="text" required />
+        <Input name="email" placeholder="Email" type="email" required />
+        <Input
+          name="password"
+          placeholder="Password"
+          type="password"
+          required
+        />
+        <Input type="submit" value="Create Account" />
+      </Form>
+    </Wrapper>
+  );
 }
