faa7cc9227c43d69bd7cdb39298581f5d6879b80
ParkKiHun <miy1205@naver.com>
2024-05-02 15:30:00 +0900

    주석 추가

--- src/components/github-btn.tsx
+++ src/components/github-btn.tsx
@@ -24,9 +24,17 @@ const Logo = styled.img`
 `;
 
 export default function GithubButton() {
+  /** useNavigate는 페이지를 이동할 때 사용된다.
+   *  Link를 사용해도 페이지를 이동할 수 있지만 단순하게 이동만 하는 경우에 Link를 사용하면 좋고
+   *  특정 이벤트가 실행됐을 때 동작하거나 추가적인 로직이 필요한 경우 useNavigate를 사용하면 좋다.
+   */
   const navigate = useNavigate();
   const onClick = async () => {
     try {
+      /**
+       * Firebase - GitHub auth provider.
+       * GitHub에는 OAuth 2.0 리디렉션이 필요하므로 리디렉션(signInWithRedirect 핸들러)을 직접 처리하거나 signInWithPopup 핸들러를 사용할 수 있다.
+       */
       const provider = new GithubAuthProvider();
       await signInWithPopup(auth, provider);
       navigate("/");
--- src/components/protected-route.tsx
+++ src/components/protected-route.tsx
@@ -1,6 +1,12 @@
 import { Navigate } from "react-router-dom";
 import { auth } from "../firebase";
 
+/**
+ * 인증 받지 않은 사용자가 접근 시 login 페이지로 이동
+ * @param children
+ * @returns children or <Navigate to="/login" />
+ */
+
 export default function ProtetedRoute({
   children,
 }: {
